8afaa51e771379f8f5a4923c8657b9893d6c1fbd
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-11-03 23:22:34 +0700

    ngrok http http://127.0.0.1:8000

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
-	// API_URL: 'https://knowledge-graph.up.railway.app/api/',
-	// URL: 'https://knowledge-graph.up.railway.app',
-	API_URL: 'http://localhost:8000/api/',
-	URL: 'http://localhost:8000',
+	API_URL: 'https://f872-42-118-53-185.ngrok-free.app/api/',
+	URL: 'https://f872-42-118-53-185.ngrok-free.app',
+	// API_URL: 'http://localhost:8000/api/',
+	// URL: 'http://localhost:8000',
 	DOMAIN_CLIENT: 'http://localhost:8080',
 }
