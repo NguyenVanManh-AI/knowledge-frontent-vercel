0b19efe0047fe9e9a0e475dea2779c621acd3cde
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-11-04 21:12:07 +0700

    ngrock

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-	API_URL: 'https://0cc0-42-118-53-185.ngrok-free.app/api/',
-	URL: 'https://0cc0-42-118-53-185.ngrok-free.app',
+	API_URL: 'https://cee6-42-118-53-185.ngrok-free.app/api/',
+	URL: 'https://cee6-42-118-53-185.ngrok-free.app',
 	// API_URL: 'http://localhost:8000/api/',
 	// URL: 'http://localhost:8000',
 	DOMAIN_CLIENT: 'http://localhost:8080',
