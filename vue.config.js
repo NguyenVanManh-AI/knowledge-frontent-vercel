fa366764410c4d3cb0f63cd90db2cbf304267792
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-11-04 23:12:03 +0700

    a

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,4 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://20.2.90.6', // Địa chỉ của API HTTP của bạn
-        changeOrigin: true,
-        secure: false,
-        pathRewrite: { '^/api': '' },
-      },
-    },
-  },
-});
+  transpileDependencies: true
+})
