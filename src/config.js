6387409188e2322311dba4b1b8755a6a720b162a
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-11-04 21:16:32 +0700

    ngrok http 8000 --host-header="localhost:8000"

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-	API_URL: 'https://cee6-42-118-53-185.ngrok-free.app/api/',
-	URL: 'https://cee6-42-118-53-185.ngrok-free.app',
+	API_URL: 'https://a4ba-42-118-53-185.ngrok-free.app/api/',
+	URL: 'https://a4ba-42-118-53-185.ngrok-free.app',
 	// API_URL: 'http://localhost:8000/api/',
 	// URL: 'http://localhost:8000',
 	DOMAIN_CLIENT: 'http://localhost:8080',
