9eff4d05509fdf7aa73977203a5d2a2f379f2bb4
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-11-05 22:31:19 +0700

    v4

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
-	// API_URL: 'https://knowledge-graph.up.railway.app/api/',
-	// URL: 'https://knowledge-graph.up.railway.app',
-	API_URL: 'http://localhost:8000/api/',
-	URL: 'http://localhost:8000',
+	API_URL: 'https://a0fe-42-118-53-185.ngrok-free.app/api/',
+	URL: 'https://a0fe-42-118-53-185.ngrok-free.app',
+	// API_URL: 'http://localhost:8000/api/',
+	// URL: 'http://localhost:8000',
 	// DOMAIN_CLIENT: 'http://localhost:8080/',
-	// DOMAIN_CLIENT: 'https://advanced-knowledge.vercel.app/',
-	DOMAIN_CLIENT: 'http://20.2.90.6/',
+	DOMAIN_CLIENT: 'https://advanced-knowledge.vercel.app/',
+	// DOMAIN_CLIENT: 'http://20.2.90.6/',
 }
