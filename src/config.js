e1c99a3af0f38db9d67791cec3cb723117ae68c4
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-11-06 09:40:46 +0700

    v10

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ export default {
 	// API_URL: 'http://localhost:8000/api/',
 	// URL: 'http://localhost:8000',
 	// DOMAIN_CLIENT: 'http://localhost:8080/',
-	DOMAIN_CLIENT: 'http://20.2.90.6/',
+	DOMAIN_CLIENT: 'https://advanced-knowledge.vercel.app/',
 	// DOMAIN_CLIENT: 'http://20.2.90.6/',
 }
