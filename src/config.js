31565576cb091a941ac722dfbc0fe22807287f25
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-11-30 18:16:49 +0700

    update10

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-	// API_URL: 'https://bfb-pbl7.xyz/api/',
-	// URL: 'https://bfb-pbl7.xyz',
-	API_URL: 'http://localhost:8000/api/',
-	URL: 'http://localhost:8000',
+	API_URL: 'https://bfb-pbl7.xyz/api/',
+	URL: 'https://bfb-pbl7.xyz',
+	// API_URL: 'http://localhost:8000/api/',
+	// URL: 'http://localhost:8000',
 	// DOMAIN_CLIENT: 'http://localhost:8080/',
 	DOMAIN_CLIENT: 'https://advanced-knowledge.vercel.app/',
 	// DOMAIN_CLIENT: 'http://20.2.90.6/',
